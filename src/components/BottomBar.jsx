f983326bf75f88a71a4d1ef5627cd76374ba0315
Tobias Thüring <t.thuering@linguala.com>
2018-06-15 14:16:19 +0200

    feature(BottomBar): add BottomBar component

---
+++ src/components/BottomBar.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const BottomBar = styled.div`
+display: flex;
+justify-content: center;
+margin: 0 20px 20px 20px;
+padding: 10px 0;
+background-color: white;
+border-radius: 4px;
+
+position: fixed;
+left: 0;
+right: 0;
+bottom: 0;
+`
+
+export default BottomBar
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as AddButton } from './components/AddButton.jsx'
+export { default as BottomBar } from './components/BottomBar.jsx'
 export { default as Button } from './components/Button.jsx'
 export { default as Logo } from './components/Logo.jsx'
