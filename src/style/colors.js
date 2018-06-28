bd72a4f12c53afe7942a13b1a290cc9d962ac758
Tobias Thüring <t.thuering@linguala.com>
2018-06-28 16:59:44 +0200

    feature(colors): add grey

--- src/style/colors.js
+++ src/style/colors.js
@@ -1,15 +1,18 @@
 const green = '#47d629'
 const poison = '#27e200'
-const white = 'white'
+const white = '#ffffff'
+const grey = '#5f5f5f';
 
 export {
   green,
+  grey,
   poison,
   white,
 }
 
 export default {
   green,
+  grey,
   poison,
   white,
 }
