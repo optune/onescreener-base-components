401a4c52a91d6c75ae7013501a188b7e6439e2b0
Tobias Thüring <tobias.thuering@oscillate.ch>
2018-07-06 16:41:36 +0200

    feature(styling): inject global styles on import of components

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import './style/global' // global styles
+
 export { default as AddButton } from './components/AddButton.jsx'
 export { default as BottomBar } from './components/BottomBar.jsx'
 export { default as Button } from './components/Button.jsx'
