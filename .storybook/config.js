097fb50420a973831e1e5ce9a27d1e3a981db502
Tobias Thüring <tobias.thuering@oscillate.ch>
2018-07-06 19:14:14 +0200

    feature(storybook): use global styles in storybook

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/react';
+import { useGlobalStyles } from '../src/index'
+
+useGlobalStyles()
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /.stories.js$/);
