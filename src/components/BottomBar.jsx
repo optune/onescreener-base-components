5fcbe2b8501c4993997da52a65aeb848b6be33d6
Tobias Thüring <tobias.thuering@oscillate.ch>
2018-07-27 11:54:11 +0200

    refactor(BottomBar): use higher specificity & rework stories

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -3,17 +3,21 @@ import styled from 'styled-components'
 const BottomBar = styled.div`
   display: flex;
   justify-content: center;
-  margin: 0 20px 20px 20px;
-  padding: 10px 0;
-  background-color: white;
+  align-items: stretch;
   border-radius: 4px;
-
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-
+  background-color: white;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.11);
+
+  /* positioning & dimensions are most important for Bottombar of SubNavigation */
+  /* FIXME: maybe use this part in SubNavigation Component? */
+  &&& {
+    margin: 0 20px 20px 20px;
+    padding: 1em 0;
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
 `
 
 export default BottomBar
--- stories/bottombar.stories.js
+++ stories/bottombar.stories.js
@@ -5,54 +5,65 @@ import { action } from "@storybook/addon-actions";
 
 import { BottomBar, Button, ButtonContainer } from "../src/index";
 
-storiesOf("Molecule/Bottombar/plain", module)
-  .add("Bottombar with text", () => <BottomBar>Text 1 2 3</BottomBar>)
-  .add("Bottombar with 1 button", () => (
-    <BottomBar>
-      <Button>Next</Button>
-    </BottomBar>
-  ))
-  .add("Bottombar with 2 buttons", () => (
-    <BottomBar>
-      <Button secondary>Previous</Button>
-      <Button>Next</Button>
-    </BottomBar>
-  ))
-  .add("Bottombar with 3 buttons", () => (
-    <BottomBar>
-      <Button secondary>Previous</Button>
-      <Button>Next</Button>
-      <Button>Preview</Button>
-    </BottomBar>
-  ));
+const HighButton = Button.extend`
+  height: 4em;
+`;
 
-storiesOf("Molecule/Bottombar/withButtonContainers", module)
-  .add("Bottombar with text", () => (
+storiesOf("Molecule/Bottombar/text", module)
+  .add("Bottombar with text", () => <BottomBar>Text 1 2 3</BottomBar>)
+  .add("Bottombar with wrapped text", () => (
     <BottomBar>
       <ButtonContainer>Text 1 2 3</ButtonContainer>
     </BottomBar>
-  ))
+  ));
+storiesOf("Molecule/Bottombar/1button", module)
   .add("Bottombar with 1 button", () => (
     <BottomBar>
+      <Button>Next</Button>
+    </BottomBar>
+  ))
+  .add("Bottombar with wrapped 1 button", () => (
+    <BottomBar>
       <ButtonContainer>
         <Button>Next</Button>
       </ButtonContainer>
     </BottomBar>
-  ))
+  ));
+
+storiesOf("Molecule/Bottombar/2buttons", module)
   .add("Bottombar with 2 buttons", () => (
     <BottomBar>
+      <Button secondary>Previous</Button>
+      <Button>Next</Button>
+    </BottomBar>
+  ))
+  .add("Bottombar with 2 wrapped buttons", () => (
+    <BottomBar>
       <ButtonContainer>
         <Button secondary>Previous</Button>
         <Button>Next</Button>
       </ButtonContainer>
     </BottomBar>
-  ))
+  ));
+
+storiesOf("Molecule/Bottombar/3buttons", module)
   .add("Bottombar with 3 buttons", () => (
+    <BottomBar>
+      <Button secondary>Previous</Button>
+      <Button>Next</Button>
+      <HighButton secondary shadow>
+        Preview
+      </HighButton>
+    </BottomBar>
+  ))
+  .add("Bottombar with 3 wrapped buttons", () => (
     <BottomBar>
       <ButtonContainer>
         <Button secondary>Previous</Button>
         <Button>Next</Button>
-        <Button>Preview</Button>
+        <HighButton secondary shadow>
+          Preview
+        </HighButton>
       </ButtonContainer>
     </BottomBar>
   ));
