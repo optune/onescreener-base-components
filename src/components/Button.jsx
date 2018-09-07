b98734cfdc1eb52dbaebdf10d4cdaf622337f3e5
Tobias Thüring <tobias.thuering@oscillate.ch>
2018-09-07 12:18:58 +0200

    refactor(Button): add disabled state & use flex model

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,23 @@
 import styled from 'styled-components'
 
-import { poison, grey, white } from '../style/colors'
+import { black, grey, poison, white } from '../style/colors.js'
 
 const StyledButton = styled.button`
-  display: block;
-  padding: ${({ round }) => round ? '0px' : '16px'};
+  display: flex;
+  justify-content: center;
+  padding: ${({ round }) => (round ? '0px' : '16px')};
   line-height: 0px;
   font-size: 1em;
   font-weight: 600;
-  color: ${({ secondary }) => (secondary ? poison : white)};
-  width:  ${({ round }) => round ? '40px' : 'auto'};
+  color: ${({ secondary, disabled }) =>
+    disabled ? grey : secondary ? poison : white};
+  width: ${({ round }) => (round ? '40px' : 'auto')};
   min-height: 40px;
-  background-color: ${({ secondary }) => (secondary ? white : poison)};
+  background-color: ${({ secondary, disabled }) =>
+    disabled ? black : secondary ? white : poison};
   border-radius: ${({ secondary, round }) =>
     round ? '50%' : secondary ? '2px' : '3px'};
-  overflow:  ${({ round }) => round ? 'hidden' : 'none'};
+  overflow: ${({ round }) => (round ? 'hidden' : 'none')};
   border-width: 0;
   border-style: solid;
   border-color: black;
