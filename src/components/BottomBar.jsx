ed90a554ef30e53b3712099a6199517c27490305
Joël Meiller <>
2019-05-22 10:04:09 +0200

    remove misc styles for BottomBar

--- src/components/BottomBar.jsx
+++ src/components/BottomBar.jsx
@@ -1,24 +1,13 @@
 import styled from 'styled-components'
 
 const BottomBar = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: stretch;
+  background: white;
   border-radius: 4px;
-  background-color: white;
   box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.11);
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
   z-index: 1;
-
-  /* positioning & dimensions are most important for Bottombar of SubNavigation */
-  /* FIXME: maybe use this part in SubNavigation Component? */
-  &&& {
-    margin: 0 20px;
-    padding: 1em 0 20px;
-    position: fixed;
-    left: 0;
-    right: 0;
-    bottom: 0;
-  }
 `
 
 export default BottomBar
