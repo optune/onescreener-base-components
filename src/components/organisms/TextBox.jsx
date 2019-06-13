7acb19591ad39cd24d6460b3c8addaf6aa295e7f
Joël Meiller <>
2019-06-13 16:20:08 +0200

    use auto-fit text for Text Box

--- src/components/organisms/TextBox.jsx
+++ src/components/organisms/TextBox.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import styled, { css } from 'styled-components'
 
 import { MediaSmall } from '../../style/media.js'
+import { AutoTextFit } from '../../utils/AutoTextFit.jsx'
 
 const Text = styled.div`
   position: relative;
@@ -15,22 +16,48 @@ const Text = styled.div`
   color: ${({ color }) => color};
   transition: color 0.3s ease-out;
 
-  & a,
-  li,
-  p,
-  h1,
+  & h1,
   h2,
   h3,
   h4,
   h5,
+  a,
+  li,
+  p,
   span {
+    font-size: 1em;
     color: ${({ color }) => color};
     white-space: pre-wrap;
     transition: color 0.3s ease-out;
+    line-height: 1.2;
+    margin: 1em 0;
+    padding: 0;
   }
 
   & a:hover {
     color: ${({ color, colorAccent }) => colorAccent || color};
+  }
+
+  & h1 {
+    font-size: 1.8em;
+    font-weight: bold;
+  }
+
+  & h2 {
+    font-size: 1.5em;
+    font-weight: bold;
+  }
+
+  & h3 {
+    font-size: 1.2em;
+  }
+
+  & h4 {
+    font-size: 1em;
+  }
+
+  & h5 {
+    font-size: 0.8em;
   }
 `
 
@@ -40,6 +67,6 @@ export const TextBox = ({ children, color, colorBackground, colorAccent }) => (
     colorBackground={colorBackground}
     colorAccent={colorAccent}
   >
-    {children} 
+    <AutoTextFit>{children}</AutoTextFit>
   </Text>
 )
