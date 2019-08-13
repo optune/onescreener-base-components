2dfbc75e66ecc0cc3cc0ba7b1baae9b134beafad
Joël Meiller <>
2019-08-13 15:20:43 +0200

    switch OpTuNE gig url to correct one

--- src/api/gigs/apis/optune.js
+++ src/api/gigs/apis/optune.js
@@ -1,8 +1,9 @@
 import { formatGigDate } from '../utils/formatGigDate.js'
 
+// const getUrl = (slug, limit, includePast) =>
+//  `https://api.optune.me/v2/events/Amazing-DJ-Joel.json?limit=${limit}&inclpast=${includePast}`
 const getUrl = (slug, limit, includePast) =>
-  `https://api.optune.me/v2/events/Amazing-DJ-Joel.json?limit=${limit}&inclpast=${includePast}`
-// const getUrl = (slug, limit, includePast) => `https://api.optune.me/v2/events/${slug}.json?limit=${limit}&inclpast=${includePast}`
+  `https://api.optune.me/v2/events/${slug}.json?limit=${limit}&inclpast=${includePast}`
 
 export const optune = {
   url: getUrl,
