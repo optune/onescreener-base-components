a4aaae57a75f763440e228382a521cfedb5b01bc
Joël Meiller <>
2020-01-15 11:48:21 +0100

    update npm packages and dependencies

--- config/shared.js
+++ config/shared.js
@@ -4,7 +4,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import globals from 'rollup-plugin-node-globals'
 import replace from 'rollup-plugin-replace'
 import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
+import url from '@rollup/plugin-url'
 
 // shared modules for different envs
 import { externals } from './externalModules.js'
