fa727386554a05e9ab1bba4a116a7b88991035df
Yurii Zaporozhchenko <yurnero017@gmail.com>
2020-01-30 20:09:36 +0200

    add sponsors

--- src/components/templates/Page.jsx
+++ src/components/templates/Page.jsx
@@ -2,6 +2,8 @@
 import React, { Fragment } from 'react'
 import styled from 'styled-components'
 
+import { Sponsors } from '../molecules/sponsors/Sponsors'
+
 import { LogoBox } from '../organisms/LogoBox.jsx'
 import { ContentBox } from '../organisms/ContentBox.jsx'
 import { LinksBox } from '../organisms/LinksBox.jsx'
@@ -61,6 +63,32 @@ export const Page = ({ page, noBacklink }) => {
     const { background, logo, content, gigAPI } = page
     const { links } = page || { links: { list: [] } }
 
+    const { font, fontURL } = page.logo.text
+
+    console.log(content)
+
+    // Importing font to page
+    let styles = document.head.getElementsByTagName('style')
+    let style
+
+    for (let i = 0; i < styles.length; i++) {
+      if (styles[i].dataset.font !== undefined) {
+        style = styles[i]
+      }
+    }
+
+    if (!style) {
+      style = document.createElement('style')
+      style.setAttribute('data-font', font)
+      style.innerHTML = fontURL + ` .apply-font {font-family: '${font}';}` // Applying font to the logo
+      document.getElementsByTagName('head')[0].appendChild(style)
+    }
+
+    if (style) {
+      style.setAttribute('data-font', font)
+      style.innerHTML = fontURL + ` .apply-font {font-family: '${font}';}` // Applying font to the logo
+    }
+
     PageComponent = (
       <Fragment>
         <GlobalStyle />
@@ -94,6 +122,7 @@ export const Page = ({ page, noBacklink }) => {
             </LinksBox>
           )}
         </PageContainer>
+        <Sponsors />
       </Fragment>
     )
   }
