63a00bb4e40ea4665c8bbb724ce78610621fd1d0
Yurii Zaporozhchenko <yurnero017@gmail.com>
2020-04-10 16:06:16 +0300

    add selection of mobile logo position

--- src/components/organisms/LogoBox.jsx
+++ src/components/organisms/LogoBox.jsx
@@ -1,9 +1,155 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import { Logo } from '../atoms/Logo.jsx'
 import { LogoText } from '../atoms/LogoText.jsx'
+
+import { MediaMobile } from '../../style/media'
+
+const stylesLogoDesktop = `
+&.desktop- {
+  &top-left {
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+
+  &top-center {
+    align-items: flex-start;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &top-right {
+    align-items: flex-start;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+
+  &center-left {
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  &center-center {
+    align-items: center;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &center-right {
+    align-items: center;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+
+  &bottom-left {
+    align-items: flex-end;
+    justify-content: flex-start;
+  }
+
+  &bottom-center {
+    align-items: flex-end;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &bottom-right {
+    align-items: flex-end;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+}`
+
+const stylesLogoMobile = `
+&.mobile- {
+  &top-left {
+    align-items: flex-start;
+    justify-content: flex-start;
+  }
+
+  &top-center {
+    align-items: flex-start;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &top-right {
+    align-items: flex-start;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+
+  &center-left {
+    align-items: center;
+    justify-content: flex-start;
+  }
+
+  &center-center {
+    align-items: center;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &center-right {
+    align-items: center;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+
+  &bottom-left {
+    align-items: flex-end;
+    justify-content: flex-start;
+  }
+
+  &bottom-center {
+    align-items: flex-end;
+    justify-content: center;
+
+    & > div > div {
+      justify-content: center;
+    }
+  }
+
+  &bottom-right {
+    align-items: flex-end;
+    justify-content: flex-end;
+
+    & > div > div {
+      justify-content: flex-end;
+    }
+  }
+}`
 
 const LogoContainer = styled.div`
   position: absolute;
@@ -16,87 +162,44 @@ const LogoContainer = styled.div`
   z-index: ${({ zIndex }) => zIndex};
   pointer-events: none;
 
-  &.top-left {
-    align-items: flex-start;
-    justify-content: flex-start;
-  }
+  ${stylesLogoDesktop}
 
-  &.top-center {
-    align-items: flex-start;
-    justify-content: center;
+  ${({ isDifferentPositions }) =>
+    isDifferentPositions &&
+    css`
+      @media ${MediaMobile} {
+        ${stylesLogoMobile}
+      }
 
-    & > div > div {
-      justify-content: center;
-    }
-  }
-
-  &.top-right {
-    align-items: flex-start;
-    justify-content: flex-end;
-
-    & > div > div {
-      justify-content: flex-end;
-    }
-  }
-
-  &.center-left {
-    align-items: center;
-    justify-content: flex-start;
-  }
-
-  &.center-center {
-    align-items: center;
-    justify-content: center;
-
-    & > div > div {
-      justify-content: center;
-    }
-  }
-
-  &.center-right {
-    align-items: center;
-    justify-content: flex-end;
-
-    & > div > div {
-      justify-content: flex-end;
-    }
-  }
-
-  &.bottom-left {
-    align-items: flex-end;
-    justify-content: flex-start;
-  }
-
-  &.bottom-center {
-    align-items: flex-end;
-    justify-content: center;
-
-    & > div > div {
-      justify-content: center;
-    }
-  }
-
-  &.bottom-right {
-    align-items: flex-end;
-    justify-content: flex-end;
-
-    & > div > div {
-      justify-content: flex-end;
-    }
-  }
+      ${({ isPreviewMobile }) =>
+        isDifferentPositions &&
+        isPreviewMobile &&
+        css`
+          ${stylesLogoMobile}
+        `}
+    `}
 `
 
-export const LogoBox = ({ logo, zIndex, getImageUrl, isPreviewMobile }) => (
-  <LogoContainer
-    className={logo.position > '' ? logo.position.toLowerCase().replace('_', '-') : 'top-center'}
-    zIndex={zIndex}
-  >
-    {logo.type === 'TEXT' ? (
-      <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
-    ) : (
-      (logo.image?.url > '' && (
-        <Logo logo={logo} getImageUrl={getImageUrl} isPreviewMobile={isPreviewMobile} />
-      )) || <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
-    )}
-  </LogoContainer>
-)
+export const LogoBox = ({ logo, zIndex, getImageUrl, isPreviewMobile }) => {
+  const classnameDesktop =
+    logo.positionDesktop > '' ? logo.positionDesktop.toLowerCase().replace('_', '-') : 'top-center'
+  const classnameMobile =
+    logo.positionMobile > '' ? logo.positionMobile.toLowerCase().replace('_', '-') : 'top-center'
+
+  return (
+    <LogoContainer
+      className={`desktop-${classnameDesktop} mobile-${classnameMobile}`}
+      zIndex={zIndex}
+      isPreviewMobile={isPreviewMobile}
+      isDifferentPositions={logo?.isDifferentPositions || false}
+    >
+      {logo.type === 'TEXT' ? (
+        <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
+      ) : (
+        (logo.image?.url > '' && (
+          <Logo logo={logo} getImageUrl={getImageUrl} isPreviewMobile={isPreviewMobile} />
+        )) || <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
+      )}
+    </LogoContainer>
+  )
+}
