86f5f5fcaf663108f8cefcb38dad9af844b92cdc
Joël Meiller <>
2020-05-18 12:36:06 +0200

    adjust logo position for other positions than bottom

--- src/components/organisms/LogoBox.jsx
+++ src/components/organisms/LogoBox.jsx
@@ -6,6 +6,7 @@ import { Logo } from '../atoms/Logo.jsx'
 import { LogoText } from '../atoms/LogoText.jsx'
 
 import { MediaMobile } from '../../style/media'
+import { useMediaQuery } from 'react-responsive'
 
 const stylesLogoDesktop = `
 &.desktop- {
@@ -158,7 +159,6 @@ const LogoContainer = styled.div`
   left: 0;
   right: 0;
   display: flex;
-  margin: var(--margin-logo);
   z-index: ${({ zIndex }) => zIndex};
   pointer-events: none;
 
@@ -177,7 +177,41 @@ const LogoContainer = styled.div`
           ${stylesLogoMobile}
         `}
     `}
+
+
+  ${({ padding }) =>
+    css`
+      ${padding}
+    `}
 `
+
+/*
+ * Defined logo padding in alignment with links
+ */
+
+const PositionLeft = ['BOTTOM_LEFT', 'CENTER_LEFT', 'TOP_LEFT']
+const PositionRight = ['BOTTOM_RIGHT', 'CENTER_RIGHT', 'TOP_RIGHT']
+const PositionBottom = ['BOTTOM_LEFT', 'BOTTOM_CENTER', 'BOTTOM_RIGHT']
+
+const defineLogoPadding = isPreviewMobile => ({
+  left: `padding-left: ${isPreviewMobile ? 4.5 : 8}rem`,
+  right: `padding-right: ${isPreviewMobile ? 4.5 : 8}rem`,
+  bottom: `padding-bottom: ${isPreviewMobile ? 4.5 : 8}rem`,
+  none: 'padding: 0',
+})
+
+const getLogoPadding = ({ logo, links, isPreviewMobile, isMobile }) => {
+  const logoPosition = isMobile && logo.positionMobile || logo.positionDesktop || logo.position
+  const paddingIndex =
+    (links.position === 'CENTER_LEFT' && PositionLeft.includes(logoPosition) && 'left') ||
+    (links.position === 'CENTER_RIGHT' && PositionRight.includes(logoPosition) && 'right') ||
+    (PositionBottom.includes(logoPosition) && 'bottom') ||
+    'none'
+
+  console.log(paddingIndex, logoPosition, links.position)
+
+  return defineLogoPadding(isPreviewMobile)[paddingIndex]
+}
 
 export const LogoBox = ({ logo, links, getImageUrl, isPreviewMobile, zIndex }) => {
   const classnameDesktop =
@@ -189,7 +223,8 @@ export const LogoBox = ({ logo, links, getImageUrl, isPreviewMobile, zIndex }) =
     (logo.position > '' && logo.position.toLowerCase().replace('_', '-')) ||
     'top-center'
 
-  const isBottomLinks = links.position.includes('BOTTOM')
+  // Media Query
+  const isMobile = useMediaQuery({ query: MediaMobile })
 
   return (
     <LogoContainer
@@ -197,17 +232,13 @@ export const LogoBox = ({ logo, links, getImageUrl, isPreviewMobile, zIndex }) =
       zIndex={zIndex}
       isPreviewMobile={isPreviewMobile}
       isDifferentPositions={logo?.isDifferentPositions || false}
+      padding={getLogoPadding({ logo, links, isPreviewMobile, isMobile })}
     >
       {logo.type === 'TEXT' ? (
         <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
       ) : (
         (logo.image?.url > '' && (
-          <Logo
-            logo={logo}
-            getImageUrl={getImageUrl}
-            isBottomLinks={isBottomLinks}
-            isPreviewMobile={isPreviewMobile}
-          />
+          <Logo logo={logo} getImageUrl={getImageUrl} isPreviewMobile={isPreviewMobile} />
         )) || <LogoText logo={logo} isPreviewMobile={isPreviewMobile} />
       )}
     </LogoContainer>
