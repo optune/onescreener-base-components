d53626f4c8e0896263d43e7f28826c630d510bee
Yurii Zaporozhchenko <yurnero017@gmail.com>
2020-06-24 12:24:10 +0300

    fix fullscreen media

--- src/components/organisms/ContentBox.jsx
+++ src/components/organisms/ContentBox.jsx
@@ -158,8 +158,8 @@ const FullscreenContainer = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  height: 100vh;
-  width: 100vw;
+  height: 100%;
+  width: 100%;
   z-index: 1;
 `
 const ResponsiveContainer = styled.div`
