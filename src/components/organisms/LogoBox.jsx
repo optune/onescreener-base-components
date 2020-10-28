8db22a54f0970ba249ed3a92228e6fd2ad9e1176
Joël Meiller <>
2020-10-28 21:33:57 +0100

    fixes for logo position on mobile

--- src/components/organisms/LogoBox.jsx
+++ src/components/organisms/LogoBox.jsx
@@ -174,9 +174,13 @@ const LogoContainer = styled.div`
     `}
 
 
-  ${({ isPreviewMobile, padding }) =>
+  ${({ isPreviewMobile, isSidePreview, padding }) =>
     css`
       ${isPreviewMobile ? padding.mobile : padding.desktop};
+
+      @media ${MediaMobile} {
+        ${!isSidePreview && padding.mobile }
+      }
     `}
 `
 
@@ -218,10 +222,10 @@ const getLogoPadding = ({ logo, links, isPreviewMobile, isSidePreview }) => {
     'none'
 
   const paddingIndexMobile =
-    (linkPosition === 'CENTER_LEFT' && PositionLeft.includes(logoPositionMobile) && 'left') ||
-    (linkPosition === 'CENTER_RIGHT' && PositionRight.includes(logoPositionMobile) && 'right') ||
-    (PositionBottom.includes(linkPosition) &&
-      PositionBottom.includes(logoPositionMobile) &&
+    // (linkPosition === 'CENTER_LEFT' && PositionLeft.includes(logoPositionMobile) && 'left') ||
+    // (linkPosition === 'CENTER_RIGHT' && PositionRight.includes(logoPositionMobile) && 'right') ||
+    //(PositionBottom.includes(linkPosition) &&
+      (PositionBottom.includes(logoPositionMobile) &&
       'bottom') ||
     (isPreviewMobile && 'bottom') ||
     'none'
