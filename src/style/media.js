3cafe75f1b5c94cbbcd07cd10b3c9779ab8621c2
McRight <yurnero017@gmail.com>
2021-02-22 16:55:48 +0200

    add MediaQuery for smaller phones

--- src/style/media.js
+++ src/style/media.js
@@ -1,4 +1,6 @@
 export const MediaSmall = '(max-width: 736px)'
 export const MediaMobile = '(max-width: 480px)'
+export const MediaSmallMobile = '(max-width: 380px)'
+
 export const NotMediaSmall = '(min-width: 736px)'
 export const NotMediaMobile = '(min-width: 481px)'
