c79c4b14ea26733c4f67aaff79e088d45226c76e
Jonathan Ferreira <jonathanferreira23@gmail.com>
2021-03-15 16:54:16 +0000

    remove scroll visibility

--- src/utils/AutoTextFit.jsx
+++ src/utils/AutoTextFit.jsx
@@ -24,6 +24,11 @@ const TextContainer = styled.div`
     !isLogo &&
     css`
       overflow-y: scroll;
+      -ms-overflow-style: none; /* Internet Explorer 10+ */
+      scrollbar-width: none; /* Firefox */
+      ::-webkit-scrollbar {
+        display: none; /* Safari and Chrome */
+      }
     `}
 `
 
