21e73e533ffb07638614a283829ecd552112c692
McRight <yurnero017@gmail.com>
2021-03-16 13:47:39 +0200

    fix content position

--- src/components/organisms/TextBox.jsx
+++ src/components/organisms/TextBox.jsx
@@ -10,14 +10,11 @@ const FadeIn = keyframes`
 `
 
 const TextBackground = styled.div`
-  // position: absolute;
-  // top: 0;
-  // bottom: 0;
-  // left: 0;
-  // right: 0;
-  width: 100%;
-  height: 100%;
-  position: relative;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
   animation: ${FadeIn} 0.3s ease-out;
 `
 
