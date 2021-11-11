3f8daf743fbd6a0007c5e7a5fed674aca7e5e7e6
Yurii Zaporozhchenko <yurnero017@gmail.com>
2021-11-11 16:15:52 +0200

    Revert "Revert "Replace icons""

---
+++ src/components/icons/platform/Onlyfans.js
@@ -0,0 +1,7 @@
+import { Onlyfans } from '@styled-icons/simple-icons/Onlyfans'
+
+import { toSquidInkIconComponent } from '../mixins/toSquidInkIcon.jsx'
+
+export const OnlyfansIcon = toSquidInkIconComponent(Onlyfans, 'onlyfans', {
+  ['no-stroke']: true,
+})
--- src/components/icons/platform/Website.jsx
+++ src/components/icons/platform/Website.jsx
@@ -1,18 +1,7 @@
-/* eslint-disable max-len, react/display-name, react/prop-types */
+import { Link } from '@styled-icons/bootstrap/Link'
 
-// Node
-import { toSquidInkIcon, defaultStrokeFill } from '../mixins/toSquidInkIcon.jsx'
+import { toSquidInkIconComponent } from '../mixins/toSquidInkIcon.jsx'
 
-// React
-import React from 'react'
-
-export const WebsiteIcon = toSquidInkIcon(() => (
-  <g id="line-arw-rt">
-    <polyline
-      {...defaultStrokeFill}
-      points="
-		24,6 42,6 42,24 	"
-    />
-    <line {...defaultStrokeFill} x1="5.9999971" y1="42" x2="41.4999962" y2="6.5" />
-  </g>
-))
+export const WebsiteIcon = toSquidInkIconComponent(Link, 'line-arw-rt', {
+  ['no-stroke']: true,
+})
