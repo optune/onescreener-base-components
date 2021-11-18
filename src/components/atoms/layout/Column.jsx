897a75117772a08d20faa791fb7f498ff50922b6
McRight <yurnero017@gmail.com>
2021-11-18 13:12:14 +0200

    get rid of react-base-components package

---
+++ src/components/atoms/layout/Column.jsx
@@ -0,0 +1,58 @@
+import classNames from 'classnames'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+
+const ColumnContainer = styled.div`
+  display: ${({ center, spaceBetween }) => (center || spaceBetween ? 'flex' : 'block')};
+  justify-content: ${({ center, spaceBetween }) =>
+    (center && 'center') || (spaceBetween && 'space-between') || 'flex-start'};
+  align-items: center;
+  flex-flow: ${({ flow }) => flow || 'column'};
+  opacity: ${({ hide }) => (hide ? 0 : 1)};
+  max-height: ${({ hide }) => (hide ? 0 : 'auto')};
+  transition: opacity 0.3s, max-height 0.3s;
+  position: ${({ relative }) => (relative ? 'relative' : 'initial')};
+`
+
+export const Column = ({
+  children,
+  className,
+  fullwidth,
+  golden13,
+  golden8,
+  half,
+  third,
+  twoThird,
+  inline,
+  ...other
+}) => (
+  <ColumnContainer
+    className={classNames('column', className, {
+      third,
+      half,
+      fullwidth,
+      inline,
+      'two-third': twoThird,
+      'golden-13': golden13,
+      'golden-8': golden8,
+    })}
+    {...other}
+  >
+    {children}
+  </ColumnContainer>
+)
+
+Column.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  hide: PropTypes.bool,
+  inline: PropTypes.bool,
+  fullwidth: PropTypes.bool,
+  golden13: PropTypes.bool,
+  golden8: PropTypes.bool,
+  half: PropTypes.bool,
+  third: PropTypes.bool,
+  twoThird: PropTypes.bool,
+}
---
+++ src/components/atoms/layout/Row.jsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames'
+
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import styled from 'styled-components'
+import { MediaSmall } from '../../../style/media'
+
+const RowContainer = styled.div`
+  width: 100%;
+  margin-top: ${({ marginTop, hide }) => (!hide && marginTop ? 1 : 0)}rem;
+  margin-bottom: ${({ marginBottom, hide }) => (!hide && marginBottom ? 3 : 0)}rem;
+  opacity: ${({ hide }) => (hide ? 0 : 1)};
+  max-height: ${({ hide }) => (hide ? 0 : 'auto')};
+  transition: opacity 0.3s, max-height 0.3s, margin-top 0.3s, margin-bottom 0.3s;
+
+  @media ${MediaSmall} {
+    margin-top: ${({ marginTop, hide }) => (!hide && marginTop ? 0.5 : 0)}rem;
+    margin-bottom: ${({ marginBottom, hide }) => (!hide && marginBottom ? 1.5 : 0)}rem;
+  }
+`
+
+export const Row = ({ children, center, className, ...other }) => (
+  <RowContainer className={classNames('row', center && 'flex-center', className)} {...other}>
+    {children}
+  </RowContainer>
+)
+
+Row.propTypes = {
+  children: PropTypes.node,
+  className: PropTypes.string,
+  center: PropTypes.bool,
+  hide: PropTypes.bool,
+}
