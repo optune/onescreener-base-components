21480414feac14700cbce9f16c86d63c25df0d11
McRight <yurnero017@gmail.com>
2021-12-29 19:40:53 +0200

    add icons to new UI

---
+++ src/components/icons/teaserLinks/Headphones.js
@@ -0,0 +1,40 @@
+// React
+import React from 'react'
+
+import { defaultStrokeFill, toSquidInkIcon } from '../mixins/toSquidInkIcon'
+
+// Teaser link - Music
+export const HeadphonesIcon = toSquidInkIcon(
+  () => (
+    <g id="headphone">
+      <path
+        {...defaultStrokeFill}
+        d="
+		M11,33H9c-2.2091389,0-4-1.7908611-4-4v-5c0-2.2091389,1.7908611-4,4-4h2c1.1045694,0,2,0.8954296,2,2v9
+		C13,32.1045685,12.1045694,33,11,33z"
+      />
+      <path
+        {...defaultStrokeFill}
+        d="
+		M4,28c-1.1045694,0-2-0.8954296-2-2s0.8954306-2,2-2"
+      />
+      <path
+        {...defaultStrokeFill}
+        d="
+		M37.5,20h2c2.2091408,0,4,1.7908611,4,4v5c0,2.2091389-1.7908592,4-4,4h-2c-1.1045685,0-2-0.8954315-2-2v-9
+		C35.5,20.8954296,36.3954315,20,37.5,20z"
+      />
+      <path
+        {...defaultStrokeFill}
+        d="
+		M44.5,25c1.1045685,0,2,0.8954296,2,2s-0.8954315,2-2,2"
+      />
+      <path
+        {...defaultStrokeFill}
+        d="
+		M9,20c0-8.2842712,6.7157288-14,15-14s15,5.7157288,15,14"
+      />
+    </g>
+  ),
+  '0 0 48 48'
+)
