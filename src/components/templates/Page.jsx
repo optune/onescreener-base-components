df388295fc65d099ab68c4f941105a84339efef3
McRight <yurnero017@gmail.com>
2022-11-08 23:36:27 +0000

    fix bg depict

--- src/components/templates/Page.jsx
+++ src/components/templates/Page.jsx
@@ -334,9 +334,16 @@ export const Page = ({
     const { links } = page || { links: { list: [] } }
     const CustomHtml = content?.customHTML > '' ? customHtml[content.customHTML] : null
 
+    console.log({ pageINSIDE: page })
     const isBackgroundSelected =
-      background?.selectedBackgroundId > '' && background?.selectedBackgroundId !== 'custom'
+      (design?.theme?.background?.selectedBackgroundId > '' &&
+        design?.theme?.background?.selectedBackgroundId !== 'custom') ||
+      (background?.selectedBackgroundId > '' && background?.selectedBackgroundId !== 'custom')
+
+    console.log({ isBackgroundSelected })
+
     const isThemeSelected = selectedThemeId > '' && selectedThemeId !== 'custom'
+    console.log({ isThemeSelected })
 
     const showRedirectOverlay = (isEditMode || !isSmall) && isSidePreview && !showStatistics
 
@@ -359,14 +366,19 @@ export const Page = ({
           className="page-container"
           preloadImage={getImageUrl(false)({
             ...background,
+            ...design?.theme?.background,
             isBackgroundSelected,
-            selectedBackgroundUrl: design?.background?.url || background?.url,
+            selectedBackgroundUrl:
+              design?.theme?.background?.url || design?.background?.url || background?.url,
           })}
           ssrDone={ssrDone}
           focusPoint={background?.focusPoint}
           fullscreen={background?.fullscreen}
-          color={background?.color}
-          designColor={isBackgroundSelected && (design?.background?.color || background?.color)}
+          color={design?.background?.color || background?.color}
+          designColor={
+            isBackgroundSelected &&
+            (design?.theme?.background?.color || design?.background?.color || background?.color)
+          }
           isPreviewMobile={isPreviewMobile}
           isSidePreview={isSidePreview}
         >
@@ -578,11 +590,16 @@ export const Page = ({
             <Background
               background={{
                 ...background,
+                ...design?.theme?.background,
                 isBackgroundSelected,
-                selectedBackgroundUrl: design?.background?.url,
+                selectedBackgroundUrl:
+                  design?.theme?.background?.url || design?.background?.url || background?.url,
               }}
-              color={background?.color}
-              designColor={isBackgroundSelected && design?.background?.color}
+              color={design?.background?.color || background?.color}
+              designColor={
+                isBackgroundSelected &&
+                (design?.theme?.background?.color || design?.background?.color || background?.color)
+              }
               getImageUrl={getUrl}
             />
           )}
