e0098070350feb43b4956ef1d9ecb5c74e6a300a
McRight <yurnero017@gmail.com>
2023-09-20 19:43:11 +0100

    replace twitter icon with new logo

--- src/components/icons/platform/Twitter.jsx
+++ src/components/icons/platform/Twitter.jsx
@@ -1,5 +1,19 @@
+import React from 'react'
 import { Twitter } from '@styled-icons/fa-brands/Twitter'
 
-import { toSquidInkIconComponent } from '../mixins/toSquidInkIcon.jsx'
+import { toSquidInkIcon, toSquidInkIconComponent } from '../mixins/toSquidInkIcon.jsx'
 
-export const TwitterIcon = toSquidInkIconComponent(Twitter, 'twitter')
+// export const TwitterIcon = toSquidInkIconComponent(Twitter, 'twitter')
+export const TwitterIcon = toSquidInkIcon(
+  () => (
+    <g id="twitter-icon">
+      <path
+        id="path1009"
+        class="cls-1"
+        d="M7.48,21.9,195.66,273.57,6.29,478.1H48.91L214.71,299l134,179.11h145L294.93,212.33,471.2,21.9H428.58L275.89,186.82,152.51,21.9ZM70.16,53.3h66.63L431,446.7H364.39Z"
+        transform="translate(-6.29 -21.9)"
+      />
+    </g>
+  ),
+  '0 0 487.43 456.19'
+)
